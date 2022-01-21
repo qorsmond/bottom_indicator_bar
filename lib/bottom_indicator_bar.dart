26a821697d80943b7ba0f17d205f19ddf1d70c20
qorsmond <qorsmond@gmail.com>
2022-01-21 22:12:39 +0200

    add svg to name

--- lib/bottom_indicator_bar.dart
+++ lib/bottom_indicator_bar.dart
@@ -1,4 +1,4 @@
 library bottom_indicator_bar;
 
-export 'package:bottom_indicator_bar/src/navigation_bar.dart';
-export 'package:bottom_indicator_bar/src/navigation_bar_item.dart';
+export 'package:bottom_indicator_bar_svg/src/navigation_bar.dart';
+export 'package:bottom_indicator_bar_svg/src/navigation_bar_item.dart';
