bbb1574382dca127dfbfaf040d3c8d33f984a4bc
qorsmond <qorsmond@gmail.com>
2022-01-21 21:43:19 +0200

    make icon prop dynamic

--- lib/src/navigation_bar_item.dart
+++ lib/src/navigation_bar_item.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
 
 class BottomIndicatorNavigationBarItem {
-  final IconData icon;
+  final dynamic icon;
   final Color backgroundColor;
 
   BottomIndicatorNavigationBarItem({
